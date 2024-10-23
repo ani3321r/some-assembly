363aa2d531397ee824175e8752ddc93dc0c2db51
ani3321r <ani3321r@gmail.com>
2024-10-23 12:57:17 +0530

    calling standard c funcs

---
+++ calling std c functions/std.s
@@ -0,0 +1,16 @@
+extern printf
+extern exit
+
+section .data
+	msg DD "Hello Raiden!",0
+	fmt DB "Output is: %s",10,0
+section .text
+
+global main
+
+main:
+	PUSH msg
+	PUSH fmt
+	CALL printf
+	PUSH 1
+	CALL exit
